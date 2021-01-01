5bb3562a43f8401b33d935f7a88b321542a92ed0
nicolaserard <62657747+nicolaserard@users.noreply.github.com>
2021-01-01 23:26:59 +0100

    Add cartesian support for position

--- SpatRevolution.js
+++ SpatRevolution.js
@@ -1,6 +1,13 @@
-/* Chataigne Module for d&b audiotechnik DS100 OSC v1.4.1 (c) Mathieu Delquignies, 10/2020
+/*
+ ==============================================================================
+
+  Chataigne Module for Spat Revolution
+
+  Copyright: Nicolas Erard, January 2021
+
+  ==============================================================================
 ===============================================================================
-This file is a Chataigne Custom Module to remote control d&b audiotechnik DS100.
+This file is a Chataigne Custom Module to remote control Flux SE Spat Revolution.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -27,8 +34,6 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /** 
  * Constants
- * 
- * Table of OSC strings from https://www.dbaudio.com/assets/products/downloads/manuals-documentation/electronics/dbaudio-osc-protocol-ds100-1.3.0-en.pdf
  * Juce Javascript don't allow const so they are defined as global variable instead, see https://docs.juce.com/master/index.html
  */
 
@@ -48,7 +53,7 @@ var OSCMessage = {
     },
     'position': function(index, value)
     {
-        if (value.getParent().cartesian.get() == 1)
+        if (Cartesian == true)
         {
             local.send("/source/" + index + "/aed", CartesianToPolar(value.get()));
         }
@@ -61,13 +66,15 @@ var OSCMessage = {
     'cartesian': function(index, value)
     {
         var positionTemp = value.getParent().position.get();
-        script.log(typeof(positionTemp[2]));
-        if (value.get() == 1)
+        script.log(positionTemp[0]);
+        if (value.get() == 1 && Cartesian == false)
         {
+            Cartesian = true;
             value.getParent().position.set(PolarToCartesian(positionTemp));
         }
         else
         {
+            Cartesian = false;
             value.getParent().position.set(CartesianToPolar(value.getParent().position.get()));
         }
     }
@@ -78,8 +85,8 @@ var OSCMessage = {
  */
 
 var numberSources = 0 ;
-var SourcesContainer;
 var Sources = [];
+var Cartesian = false;
 //var Edit = false;
 
 /* 	===============================================================================
@@ -100,7 +107,9 @@ function init()
     
     LenSources = SetupContainer.addTrigger("Ask Sources number", "Ask for the number of sources");
     numberSourcesParameter = SetupContainer.addIntParameter("Sources number", "Define the number of sources in Chataigne (automatically update from Spat).", numberSources, 0, 256);
-    
+
+    var CartesianOrPolar = SetupContainer.addBoolParameter("Cartesian", "Polar or cartesian", false);
+
 
     local.send("/global/project/source/count", 0);
 }
@@ -138,6 +147,27 @@ function moduleValueChanged(value)
         if (value.name == 'sourcesNumber')
         {
             setSourcesNumber(value.get());
+        }
+        else if (value.name == 'cartesian')
+        {
+            if (value.get() == 1 &&  Cartesian == false)
+            {
+                Cartesian = true;
+                for (var i=0; i<=Sources.length;i++)
+                {
+                    Sources[i].position.set(PolarToCartesian(Sources[i].position.get()));
+                }
+            }
+            else
+            {
+                Cartesian = false;
+                for (var i=0; i<=Sources.length;i++)
+                {
+                    Sources[i].position.set(CartesianToPolar(Sources[i].position.get()));
+                }
+
+
+            }
         }
         else
         {
@@ -204,30 +234,44 @@ function oscGlobalEvent(address, args)
 
 function oscSourceEvent(address, args)
 {
-    script.log(args.length);
-    var source = Sources[parseInt(address[2])-1];
+    var i = parseInt(address[2])-1;
+    var source = Sources[i];
     if (address[3]=='gain')
+
     {
-        source.gain.set(args[0]);
+        if (typeof(args[0]) == 'number')
+        {
+            script.log('c\'est le gain');
+
+            source.gain.set(args[0]);
+        }
     }
     if (address[3]=='lfe')
     {
-        source.lfe.set(args[0]);
+        if (typeof(args[0]) == 'number')
+        {
+            source.lfe.set(args[0]);
+        }
     }
     if (address[3] =='mute')
     {
-        source.mute.set(args[0]);
+        if (typeof(args[0]) == 'number')
+        {
+            source.mute.set(args[0]);
+        }
     }
     if (address[3] =='solo')
     {
-        source.solo.set(args[0]);
+        if (typeof(args[0]) == 'number')
+        {
+            source.solo.set(args[0]);
+        }
     }
     if (address[3]=='aed')
     {
-        if (typeof(args[0]) == 'float' && typeof(args[1]) == 'float' && typeof(args[2]) == 'float')
+        if (typeof(args[0]) == 'number' && typeof(args[1]) == 'number' && typeof(args[2]) == 'number')
         {
-            script.log(args);
-            if (source.cartesian.get() == 1)
+            if (Cartesian == true)
             {
                 source.position.set(PolarToCartesian(args));
             }
@@ -238,12 +282,6 @@ function oscSourceEvent(address, args)
         }
     }
 }
-
-/**
- * Set the spread parameter of a specific sound object by its matrix input number
- * @param {integer} object
- * @param {float} spread
- */
 
 function createSource(index)
 {
@@ -267,22 +305,19 @@ function createSource(index)
     var lfe = source.addFloatParameter("LFE", "LFE level", -144.5, -144.5, 24);
     lfe.setAttribute("readonly", true);
 
-    var CartesianOrPolar = source.addBoolParameter("Cartesian", "Polar or cartesian", false);
-
     var position = source.addPoint3DParameter("Position", "Position", [0.0,0.0,2.0]);
 
     //var edit = source.addBoolParameter("Edit", "Edit", 0);
 
     local.scripts.setCollapsed(true);
 
-    local.send("/source/"+index+"/dump", 0);
     return source
     //script.log(Sources.length);
 }
 
 function delSource(index)
 {
-    script.log('Remove source index: ' + index+1 + ' over number source ' + Sources.length);
+    script.log('Remove source index: ' + index+1 + ', name: ' + Sources[index].name);
     local.values.removeContainer(Sources[index].name);
     local.scripts.setCollapsed(true);
 
@@ -295,29 +330,25 @@ function setSourcesNumber(number)
     {
         while (numberSources < number)
         {
-
             numberSources += 1;
             Sources.push(createSource(numberSources));
+            local.send("/source/"+numberSources+"/dump", 0);
+
         }
     }
     else if (numberSources > number)
     {
-        numberSources -= 1;
-        delSource(numberSources);
+        while (numberSources > number)
+        {
+            numberSources -= 1;
+            delSource(numberSources);
+        }
     }
-
-    var text = 'Coucou' ;
-   for (var i = 0; i < Sources.length; i++)
-   {
-        text += Sources[i].name;
-    }
-    script.log(text);
 
 }
 
 function CartesianToPolar(value)
 {
-
     var positionAED = [0,0,0];
     if (value[0] !=0 || value[1] != 0 || value[2] !=0 )
     {
@@ -352,8 +383,9 @@ function CartesianToPolar(value)
 
 function PolarToCartesian(value)
 {
-    var positionXYZ = [0,0,0];
-    script.log(value[0]);
+    var positionXYZ = [0.0,0.0,0.0];
+    script.log('value: ' + value[2] + ', type: ' + typeof(value[2]));
+    script.log(typeof(Math.cos(DegToRad(value[0]))));
     positionXYZ[0] = value[2] * Math.cos(DegToRad(value[0])) * Math.sin(DegToRad(value[1]));
     positionXYZ[1] = value[2] * Math.sin(DegToRad(value[0])) * Math.sin(DegToRad(value[1]));
     positionXYZ[2] = value[2] * Math.cos(DegToRad(value[1]));
@@ -363,5 +395,5 @@ function PolarToCartesian(value)
 
 function DegToRad(value)
 {
-    return - Math.PI * (value-90) / 180;
+    return -1 * Math.PI * (value-90) / 180;
 }
